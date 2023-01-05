b300f9cd2f6e3ce6a0bec4d6883df385e6b3c018
Kuba Jałoszyński <60043296+Xsarius@users.noreply.github.com>
2023-01-05 14:55:33 +0100

    update

--- web/web/settings.py
+++ web/web/settings.py
@@ -69,7 +69,7 @@ ROOT_URLCONF = 'web.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'your_app_name', 'templates', 'your_app_name'),],
+        'DIRS': [os.path.join(BASE_DIR, 'webcontrol', 'templates', 'webcontrol'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
