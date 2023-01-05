e70983ed4eaeb05b390acdc442439229c96881c2
Kuba Jałoszyński <60043296+Xsarius@users.noreply.github.com>
2023-01-05 13:24:06 +0100

    Update

--- web/startup.sh
+++ web/startup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-sh ./rpi_pins_setup.sh
+#sh ./rpi_pins_setup.sh
 
 python3 manage.py makemigrations
 python3 manage.py migrate
-gunicorn web.wsgi:application --bind 0.0.0.0:8000
+#gunicorn web.wsgi:application --bind 0.0.0.0:8000
