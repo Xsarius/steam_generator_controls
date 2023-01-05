d337b30247cb899949567bf1a041b8d1fa9b1254
Kuba Jałoszyński <60043296+Xsarius@users.noreply.github.com>
2023-01-05 16:00:21 +0100

    update

--- web/startup.sh
+++ web/startup.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 #sh ./rpi_pins_setup.sh
-redis-server
 
 python3 manage.py makemigrations
 python3 manage.py migrate
-
-redis-cli ping
 # gunicorn web.wsgi:application --bind 0.0.0.0:8000
