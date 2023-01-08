556d7a0636502133a703f8a3f5bacb97a2da2cd2
Kuba Jałoszyński <60043296+Xsarius@users.noreply.github.com>
2023-01-08 21:45:23 +0100

    update

--- web/webcontrol/tasks.py
+++ web/webcontrol/tasks.py
@@ -16,7 +16,7 @@ class SGController:
         self.pressure = pressure
         self.control_commands = commands
 
-    @app.task1()
+    @app.task()
     def control_loop(self):
         print("Controls started")
 
@@ -101,7 +101,7 @@ class SGController:
         self.control_commands['STOP'] = commands['STOP']
         self.control_commands['save'] = commands['save']
 
-    @app.task1()
+    @app.task()
     def save_data_to_db(self, data):
         # SteamGenerator.objects.create(
         #     water_temp= self.temp[0],
