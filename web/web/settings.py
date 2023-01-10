3f042b3b58e5d7e4c9269679f4bdc03d94534395
Kuba Jałoszyński <60043296+Xsarius@users.noreply.github.com>
2023-01-10 09:14:59 +0100

    update

--- web/web/settings.py
+++ web/web/settings.py
@@ -139,5 +139,7 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CELERY_BROKER_URL = "redis://redis-broker:6379/0"
-CELERY_RESULT_BACKEND = "redis://redis-broker:6379/0"
+CELERY_BROKER_URL = "redis://redis:6379/0"
+CELERY_BROKER_TRANSPORT = 'redis'
+CELERY_RESULT_BACKEND = "redis"
+CELERY_BACKEND_URL = 'redis://redis:6379/1'
