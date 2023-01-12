1c8462ef0f5bd20a3bf1f16df0da4d2c4c378eb3
Kuba Jałoszyński <60043296+Xsarius@users.noreply.github.com>
2023-01-12 09:27:47 +0100

    update

--- web/web/celery.py
+++ web/web/celery.py
@@ -5,6 +5,6 @@ from celery import Celery
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "web.settings")
 
-app = Celery("web", 'redis://redis:6379/0')
+app = Celery("web", broker='redis://redis:6379/0')
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
